4b1cca7458a73011b431a7e7bca4fb80b77a9bfc
woodelin-florveus <woodelinflorveus@gmail.com>
2020-12-08 18:01:14 -0500

    commit tree 6161c4a30c55c22e757a3227e3b38f53c68fe632

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
 # code here!
+
+require "pry"
+
+class School
+    
+    attr_accessor :roster
+    
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end
+
+    def grade(grade)
+        # binding.pry
+        @roster[grade] ||= @roster
+    end
+
+    def sort
+
+        new_hash = {}
+
+        @roster.each do |key, value|
+            # binding.pry
+            new_hash[key] = value.sort
+        end        
+            new_hash
+    end
+
+    # expected: {7=>["Blake Johnson", "Jack Bauer"], 9=>["Bart Simpson", "Homer Simpson"], 10=>["Avi Flombaum", "Jeff Baird"]}
+
+
+end
